296cff8267c1215726feeb7f9067521d7a800f55
FranciscoCheca06 <137060007+FranciscoCheca06@users.noreply.github.com>
2023-11-30 19:21:33 +0100

    Update Main.java

--- src/mastrotadatabase/Main.java
+++ src/mastrotadatabase/Main.java
@@ -48,14 +48,14 @@ public class DatabaseConnector implements AutoCloseable {
         crearTabla("RawMaterial", "ID INT(6) PRIMARY KEY, Name_of_material VARCHAR2(20), length NUMBER, height NUMBER, width NUMBER, Weight NUMBER"); 
         crearTabla("Car", "Serial_number INT(6) PRIMARY KEY, name_of_the_car VARCHAR2(20), power INT(3), engine_displacement INT(4)"); 
         crearTabla("MakeCar", "ID_raw_material INT(6), Serial_number INT(6), PRIMARY KEY(ID_raw_material, Serial_number), FOREIGN KEY (ID_raw_material) REFERENCES RawMaterial(ID), FOREIGN KEY (Serial_number) REFERENCES Car(Serial_number)"); 
-        crearTabla("Empleado", "Telefono VARCHAR2(15) PRIMARY KEY, Numero_de_empleado INT(5) PRIMARY KEY, DNI VARCHAR2(20) PRIMARY KEY, Nombre VARCHAR2(100), Direccion VARCHAR2(255), Fecha_de_contratacion DATE"); 
+        crearTabla("Empleado", "Telefono VARCHAR2(15), Numero_de_empleado INT(5) PRIMARY KEY, DNI VARCHAR2(20), Nombre VARCHAR2(100), Direccion VARCHAR2(255), Fecha_de_contratacion DATE"); 
         crearTabla("Contrato", "Numero_de_contrato INT(6) PRIMARY KEY, Fecha_de_inicio DATE, Fecha_de_fin DATE, Horario VARCHAR2(100), Sueldo NUMBER, Departamento VARCHAR2(50)");
-        crearTabla("Tiene", "Telefono VARCHAR2(15), Numero_de_empleado INT(5), DNI VARCHAR2(20), Numero_de_contrato INT(6), PRIMARY KEY(Numero_de_contrato, Numero_de_empleado), PRIMARY KEY(Numero_de_contrato, DNI), PRIMARY KEY(Numero_de_contrato, Telefono), FOREIGN KEY (Telefono, Numero_de_empleado, DNI) REFERENCES Empleado(Telefono, Numero_de_empleado, DNI), FOREIGN KEY (Numero_de_contrato) REFERENCES Contrato(Numero_de_contrato) ");
-        crearTabla("Gasto", "Descripcion TEXT, Cantidad NUMBER, ID_gasto INT(6) PRIMARY KEY, Categoria VARCHAR2(20)");//
-        crearTabla("Supplier", "Cif VARCHAR2(10) PRIMARY KEY, Denomination VARCHAR2(20), Address VARCHAR2(50)");//
-        crearTabla("Clients", "Cif VARCHAR2(10) PRIMARY KEY, Denomination VARCHAR2(20), Address VARCHAR2(50), Type VARCHAR2(10)");//
-        crearTabla("Order", "Cif VARCHAR2(10), Serial_number INT(10), Price NUMBER, FOREIGN KEY (Cif) REFERENCES Clients(Cif), FOREIGN KEY (Serial_number) REFERENCES Car(Serial_number) "); //
-        crearTabla("Purchase", "Cif VARCHAR2(10), ID_raw_material INT(6), Price NUMBER, FOREIGN KEY (Cif) REFERENCES Supplier(Cif), FOREIGN KEY (ID_raw_material) REFERENCES RawMaterial(ID)"); //
+        crearTabla("Tiene", "Numero_de_empleado INT(5), Numero_de_contrato INT(6), PRIMARY KEY(Numero_de_contrato, Numero_de_empleado), FOREIGN KEY (Numero_de_empleado) REFERENCES Empleado(Numero_de_empleado), FOREIGN KEY (Numero_de_contrato) REFERENCES Contrato(Numero_de_contrato) ");
+        crearTabla("Gasto", "Descripcion TEXT, Cantidad NUMBER, ID_gasto INT(6) PRIMARY KEY, Categoria VARCHAR2(20)");
+        crearTabla("Supplier", "Cif VARCHAR2(10) PRIMARY KEY, Denomination VARCHAR2(20), Address VARCHAR2(50)");
+        crearTabla("Clients", "Cif VARCHAR2(10) PRIMARY KEY, Denomination VARCHAR2(20), Address VARCHAR2(50), Type VARCHAR2(10)");
+        crearTabla("Order", "Cif VARCHAR2(10), Serial_number INT(10), Price NUMBER, FOREIGN KEY (Cif) REFERENCES Clients(Cif), FOREIGN KEY (Serial_number) REFERENCES Car(Serial_number) ");
+        crearTabla("Purchase", "Cif VARCHAR2(10), ID_raw_material INT(6), Price NUMBER, FOREIGN KEY (Cif) REFERENCES Supplier(Cif), FOREIGN KEY (ID_raw_material) REFERENCES RawMaterial(ID)");
     }
 
     private void crearTabla(String nombreTabla, String columnas) {
