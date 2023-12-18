dd697f654ec06543b7cea65ceaa96c71f29f5332
Juan Sánchez Fernández <j.sanchezfedez@gmail.com>
2023-12-18 13:28:23 +0100

    La aplicación ya crea las tablas al iniciar la aplicación y las elimina al cerrarla

--- src/mastrotadatabase/DatabaseConnector.java
+++ src/mastrotadatabase/DatabaseConnector.java
@@ -1,62 +1,125 @@
 package mastrotadatabase;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.ResultSet;
 
-/**
- *
- * @author juan
- */
-
-public class DatabaseConnector implements AutoCloseable{
+public class DatabaseConnector implements AutoCloseable {
     private Connection connection;
     private String url = "jdbc:oracle:thin:@oracle0.ugr.es:1521/practbd.oracle0.ugr.es";
     private String usuario = "x7766471";
     private String contraseña = "x7766471";
 
-    public DatabaseConnector() throws SQLException {
+    public DatabaseConnector() {
         try {
-            // Asegúrate de que el driver JDBC de Oracle esté cargado
             Class.forName("oracle.jdbc.driver.OracleDriver");
-            // Establece la conexión con la base de datos
             this.connection = DriverManager.getConnection(url, usuario, contraseña);
             System.out.println("Conexión exitosa a la base de datos.");
         } catch (ClassNotFoundException ex) {
-            System.err.println("El driver JDBC de Oracle no se encontró en el classpath.");
-            ex.printStackTrace();
+            throw new RuntimeException("El driver JDBC de Oracle no se encontró en el classpath.", ex);
         } catch (SQLException ex) {
             System.err.println("No se pudo establecer la conexión con la base de datos.");
             ex.printStackTrace();
-            throw ex; // Re-lanza la excepción para manejarla más arriba si es necesario
+            throw new RuntimeException("Error al establecer la conexión con la base de datos.", ex);
         }
     }
 
     public Connection getConnection() {
         return this.connection;
     }
+    
+    private boolean existeTabla(Statement stmt, String nombreTabla) throws SQLException{
+        ResultSet rs = stmt.executeQuery("SELECT COUNT(*) FROM user_tables WHERE table_name = '" + nombreTabla + "'");
+        rs.next();
+        if (rs.getInt(1) > 0) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    /**
+     * @brief Crea una tabla concreta
+     * @param stmt
+     * @param nombreTabla
+     * @param sqlCreacion
+     * @throws SQLException 
+     */
+    private void crearTabla(Statement stmt, String nombreTabla, String sqlCreacion) throws SQLException {
+        if (!existeTabla(stmt, nombreTabla)) {
+            stmt.execute(sqlCreacion);
+        }
+    }
+    
+    /**
+     * @brief Elimina una tabla concreta.
+     * @param stmt
+     * @param nombreTabla
+     * @throws SQLException 
+     */
+    private void eliminarTabla(Statement stmt, String nombreTabla) throws SQLException {
+        if(existeTabla(stmt, nombreTabla))
+            stmt.execute("DROP TABLE " + nombreTabla);
+    }
+    
 
-     // Implementa el método close de la interfaz AutoCloseable
+    /**
+     * @brief Crea todas las tablas de la base de datos
+     */
+    public void crearTablasSiNoExisten() {
+    try (Statement stmt = connection.createStatement()) {
+        crearTabla(stmt, "EMPLEADO", "CREATE TABLE empleado(n_empleado NUMBER GENERATED ALWAYS AS IDENTITY, DNI VARCHAR2(20), telefono VARCHAR2(15), direccion VARCHAR2(255), fecha_contratacion DATE, departamento VARCHAR2(255), PRIMARY KEY(n_empleado))");
+        crearTabla(stmt, "CONTRATO", "CREATE TABLE contrato(n_contrato NUMBER GENERATED ALWAYS AS IDENTITY, departamento VARCHAR2(255), fecha_inicio DATE, duracion INT, estado VARCHAR2(8) CHECK(estado IN('activo','inactivo')), n_empleado NUMBER, PRIMARY KEY(n_contrato), FOREIGN KEY(n_empleado) REFERENCES empleado(n_empleado) ON DELETE SET NULL)");
+        crearTabla(stmt, "SUPPLIER", "CREATE TABLE supplier(CIF_supp VARCHAR2(20), denomination VARCHAR2(255), address VARCHAR2(255), type VARCHAR2(255), PRIMARY KEY (CIF_supp))");
+        crearTabla(stmt, "CLIENT", "CREATE TABLE client(CIF_client VARCHAR2(20), denomination VARCHAR2(255), address VARCHAR2(255), type VARCHAR2(255), PRIMARY KEY(CIF_client))");
+        crearTabla(stmt, "MATERIAL", "CREATE TABLE material(material_id NUMBER GENERATED ALWAYS AS IDENTITY, name VARCHAR2(255), length NUMBER, height NUMBER, width NUMBER, weight NUMBER, PRIMARY KEY(material_id))");
+        crearTabla(stmt, "PURCHASE", "CREATE TABLE purchase(purchase_id NUMBER GENERATED ALWAYS AS IDENTITY, price NUMBER, quantity NUMBER, material_id NUMBER, CIF_supp VARCHAR2(20), PRIMARY KEY (purchase_id), FOREIGN KEY(material_id) REFERENCES material(material_id) ON DELETE SET NULL, FOREIGN KEY(CIF_supp) REFERENCES supplier(CIF_supp) ON DELETE SET NULL)");
+        crearTabla(stmt, "GASTO", "CREATE TABLE gasto(id_gasto NUMBER GENERATED ALWAYS AS IDENTITY, cantidad NUMBER, fecha DATE, categoria VARCHAR2(255), purchase_id NUMBER, n_contrato NUMBER, PRIMARY KEY(id_gasto), FOREIGN KEY (purchase_id) REFERENCES purchase(purchase_id) ON DELETE CASCADE, FOREIGN KEY (n_contrato) REFERENCES contrato(n_contrato) ON DELETE CASCADE)");
+        crearTabla(stmt, "CAR", "CREATE TABLE car(serial_n VARCHAR2(255), name VARCHAR2(255), power NUMBER, engine_disp NUMBER, PRIMARY KEY(serial_n))");
+        crearTabla(stmt, "CAR_MATERIAL", "CREATE TABLE car_material(serial_n VARCHAR2(255), material_id NUMBER, FOREIGN KEY (serial_n) REFERENCES car(serial_n) ON DELETE CASCADE, FOREIGN KEY (material_id) REFERENCES material(material_id) ON DELETE CASCADE, PRIMARY KEY(serial_n, material_id))");
+        crearTabla(stmt, "ORDER_", "CREATE TABLE order_(order_id NUMBER GENERATED ALWAYS AS IDENTITY, price NUMBER, serial_n VARCHAR2(255), CIF_client VARCHAR(20), PRIMARY KEY (order_id), FOREIGN KEY (serial_n) REFERENCES car(serial_n) ON DELETE CASCADE, FOREIGN KEY (CIF_client) REFERENCES client(CIF_client) ON DELETE CASCADE)");
+        crearTabla(stmt, "INGRESO", "CREATE TABLE ingreso(ingreso_id NUMBER GENERATED ALWAYS AS IDENTITY, cantidad NUMBER, fecha DATE, categoria VARCHAR2(255), order_id NUMBER, PRIMARY KEY(ingreso_id), FOREIGN KEY(order_id) REFERENCES order_(order_id) ON DELETE CASCADE)");
+        
+        System.out.println("Tablas creadas o verificadas exitosamente.");
+    } catch (SQLException e) {
+        System.err.println("Error al crear las tablas: " + e.getMessage());
+        e.printStackTrace();
+    }
+}
+    /**
+     * @brief Elimina todas las tablas de la base de datos
+     */
+    public void eliminarTablasSiExisten() {
+        try (Statement stmt = connection.createStatement()) {
+            // Eliminar tablas
+            eliminarTabla(stmt, "INGRESO");   
+            eliminarTabla(stmt, "ORDER_");   
+            eliminarTabla(stmt, "CAR_MATERIAL");   
+            eliminarTabla(stmt, "CAR");   
+            eliminarTabla(stmt, "GASTO");   
+            eliminarTabla(stmt, "PURCHASE");   
+            eliminarTabla(stmt, "MATERIAL");   
+            eliminarTabla(stmt, "CLIENT");   
+            eliminarTabla(stmt, "SUPPLIER");   
+            eliminarTabla(stmt, "CONTRATO");   
+            eliminarTabla(stmt, "EMPLEADO");
+            
+            System.out.println("Tablas eliminadas exitosamente.");
+        } catch (SQLException e) {
+            System.err.println("Error al eliminar las tablas: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void close() throws SQLException {
         if (this.connection != null && !this.connection.isClosed()) {
+            eliminarTablasSiExisten();
             this.connection.close();
             System.out.println("Conexión cerrada exitosamente.");
         }
     }
-    
-    public void crearTabla() {
-    String sqlCreate = "CREATE TABLE ejemplo (" +
-                       "id INT PRIMARY KEY, " +
-                       "nombre VARCHAR2(100), " +
-                       "descripcion VARCHAR2(255))";
-    try (Statement stmt = this.connection.createStatement()) {
-        stmt.execute(sqlCreate);
-        System.out.println("Tabla creada exitosamente.");
-    } catch (SQLException e) {
-        e.printStackTrace();
-        System.err.println("Error al crear la tabla: " + e.getMessage());
-    }
-    }
 }
-
